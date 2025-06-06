76886e358707315c2da49e3767d40514eba4b382
Yiming <2018565194@qq.com>
2025-06-06 23:03:31 +0800

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,1 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include"Date.h"
+int main()
+{
+	Date d1(2025,4,24);
+	Date d2 = d1 + 100;
+	d1.Print();
+	d2.Print();
+	Date d3 = d1 += 100;
+	d1 = d3;
+	d3.Print();
+	d1.Print();
+	return 0;
+
+}
