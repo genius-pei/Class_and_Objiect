9ef787650b5b3408e170cfcae76d36f8d4f78bdf
Yiming <2018565194@qq.com>
2025-06-07 22:09:29 +0800

    Update Date.h

--- Date.h
+++ Date.h
@@ -10,7 +10,7 @@ public:
 	{
 		assert(month > 0 && month < 13);
 		static int monthdayarr[13] = { -1,31,28,31,30,31,30,31,30,31,31,30,31 };
-		if (month == 2 && (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))//为闰年的情况
+		if (month == 2 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)))//为闰年的情况
 		{
 			return 29;
 		}
@@ -21,15 +21,37 @@ public:
 	void  Print();
 
 	Date(int year = 1, int month = 1, int day = 1);//构造函数
-
-	Date& operator+=(int day);
-	Date operator+(int day);
-	/*Date& operator-=(int day);*/
-	Date& operator=(const Date& d)
+	Date(const Date& d)
 	{
 		_year = d._year;
 		_month = d._month;
 		_day = d._day;
+	}
+	Date& operator+=(int day);
+	Date operator+(int day);
+	Date& operator-=(int day);
+	Date operator-(int day);
+	Date& operator++();//前置++
+	Date operator++(int);//后置++
+	Date& operator--();//前置——
+	Date operator--(int);//后置——
+	bool  operator==(const Date& d);
+	bool  operator!=(const Date& d);	
+	bool  operator<(const Date& d);
+	bool operator<=(const Date& d);
+	bool  operator>(const Date& d);
+	bool  operator>=(const Date& d);
+
+
+
+	Date& operator=(const Date& d)
+	{
+		if (this != &d)
+		{
+			_year = d._year;
+			_month = d._month;
+			_day = d._day;
+		}
 		return *this;
 	}
 
