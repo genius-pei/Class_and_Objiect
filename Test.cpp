baa36186b4edded13e0d50eb4e42f497c59b773d
Yiming <2018565194@qq.com>
2025-06-07 22:09:33 +0800

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -1,15 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include"Date.h"
-int main()
+void test01()
 {
-	Date d1(2025,4,24);
-	Date d2 = d1 + 100;
+	Date d1(2025, 4, 24);
+	Date d2 = d1 += 100;
 	d1.Print();
 	d2.Print();
-	Date d3 = d1 += 100;
-	d1 = d3;
-	d3.Print();
+	//Date d3 = d1 += 100;
+	//d1 = d3;
+	//d3.Print();
+	//d1.Print();
+}
+void test02()
+{
+	Date d1(2025, 4, 24);
+	Date d2 = d1 - 1000;
 	d1.Print();
-	return 0;
+	d2.Print();
+}
+void test03()
+{
+	Date d1(2025, 4, 24);
+	Date d2 =++d1;
+	d1.Print();
+	d2.Print();
 
+}
+void test04()
+{
+	Date d1(2025, 4, 24);
+	Date d2(2025, 4, 21);
+	cout << (d1 < d2) << endl;
+	cout << (d1 > d2) << endl;
+}
+
+int main()
+{
+	//test01();
+	//test02();
+	//test03();
+	test04();
+	return 0;
 }
